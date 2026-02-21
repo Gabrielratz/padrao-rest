55afc755ba3ba77954a6c3914192a6c7492521f3
Gabrielratz <gabrielpavan08@hotmail.com>
2026-02-21 00:10:40 -0300

    atualização_db

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import postgres from "postgres";
 
 
-const URL = 'postgresql://neondb_owner:npg_XjmIu4xQ2khs@ep-still-block-ad15o918-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+const URL = process.env.DATABASE_URL
 export const sql = postgres(URL,{ ssl: 'require'
 
 })
